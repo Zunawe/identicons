49348f6306c8d0dc488a70c9b9278c91d1b6e77f
Zunawe <gyroscope15@gmail.com>
2017-04-19 15:22:38 -0600

    Add function comments to squareIdenticon

--- squareIdenticon.js
+++ squareIdenticon.js
@@ -1,5 +1,17 @@
+/**
+ * @callback hasher
+ * @param {String} input - The string to be hashed
+ * @return {String} - A string in hexadecimal of some hash
+ */
+
+/**
+ * Turns an id and a hashing function into a unique square identicon SVG element
+ * @param {Number} - Side length in pixels of the image
+ * @param {String} id - The id to be hashed
+ * @param {hasher} hashFunction - A function that hashes strings (e.g. md5, sha-1, etc...)
+ */
 function squareIdenticonSVG(size, id, hashFunction){
-	var idHash = string2ByteArray(hashFunction(id));
+	var idHash = string2ByteArray(hashFunction(id.toString()));
 
 	var svgNS = 'http://www.w3.org/2000/svg';
 	var svg = document.createElementNS(svgNS, 'svg');
@@ -43,6 +55,11 @@ function squareIdenticonSVG(size, id, hashFunction){
 	return svg;
 }
 
+/**
+ * Turns a string of hexadecimal into an array of byte-sized numbers
+ * @param {String} string - The input to convert
+ * @return {Number[]} - An array with size half the length of the input string
+ */
 function string2ByteArray(string){
 	var bytes = [];
 	for(var i = 0; i < string.length / 2; ++i){
@@ -51,6 +68,12 @@ function string2ByteArray(string){
 	return bytes;
 }
 
+/**
+ * Gets the nth bit from an array of bytes
+ * @param {Number} n - The index of the bit (0 is the lowest-order bit, 8 is the first bit of the second byte, etc...)
+ * @param {Number[]} bytes - The array of bytes to look through
+ * @return {Number} - 0 or 1
+ */
 function getBit(n, bytes){
 	var byteIndex = Math.floor(n / 8) % bytes.length;
 	var bitIndex = 7 - ((n % (8 * bytes.length)) - (byteIndex * 8));
@@ -58,6 +81,12 @@ function getBit(n, bytes){
 	return (bytes[byteIndex] & (0x01 << bitIndex)) >> bitIndex;
 }
 
+/**
+ * Turns a number into a string padded on the left with zeroes until it's at least some length
+ * @param {Number} base - Convert the number to this base
+ * @param {Number} size - The width of the output
+ * @return {String}
+ */
 Number.prototype.padFront = function (base, size){
 	var s = this.toString(base);
 	while(s.length < size){
