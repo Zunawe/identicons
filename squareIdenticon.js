6e16a53b7e514e82a439de517cbc598236e7242b
Zunawe <gyroscope15@gmail.com>
2017-04-19 15:49:55 -0600

    Add more comments

--- squareIdenticon.js
+++ squareIdenticon.js
@@ -9,18 +9,24 @@
  * @param {Number} - Side length in pixels of the image
  * @param {String} id - The id to be hashed
  * @param {hasher} hashFunction - A function that hashes strings (e.g. md5, sha-1, etc...)
+ * @return {Object} - An SVG element object
  */
 function squareIdenticonSVG(size, id, hashFunction){
 	var idHash = string2ByteArray(hashFunction(id.toString()));
 
+	// Set up the SVG object to return
 	var svgNS = 'http://www.w3.org/2000/svg';
 	var svg = document.createElementNS(svgNS, 'svg');
 
+	// Create known attributes
+	// crispEdges prevents seams between blocks (we know everything is 90 degree angles anyway)
 	svg.setAttribute('width', size);
 	svg.setAttribute('height', size);
 	svg.setAttribute('xmlns', svgNS);
 	svg.setAttribute('shape-rendering', 'crispEdges');
 
+	// Generate the map of whether a particular position will be filled or empty
+	// Inner loop stops at 3 for symmetry
 	var pixelMap = [[], [], [], [], []];
 	for(var i = 0; i < 5; ++i){
 		for(var j = 0; j < 3; ++j){
@@ -30,12 +36,15 @@ function squareIdenticonSVG(size, id, hashFunction){
 		pixelMap[i][4] = pixelMap[i][0];
 	}
 
+	// Color is just the rgb hex of the last 3 bytes
+	// Size is distributed to have half a box width on every edge
 	var fillColor = '#' + idHash[idHash.length - 3].padFront(16, 2) +
 	                      idHash[idHash.length - 2].padFront(16, 2) +
 	                      idHash[idHash.length - 1].padFront(16, 2);
 	var boxSize = Math.floor(size / 6);
 	var marginSize = Math.floor((boxSize / 2) + ((size % 6) / 2));
 
+	// Loop through the map and append a rectangle to the SVG if necessary
 	for(var i = 0; i < 5; ++i){
 		for(var j = 0; j < 5; ++j){
 			if(pixelMap[i][j]){
